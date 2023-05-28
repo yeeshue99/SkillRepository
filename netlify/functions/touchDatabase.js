946d9dacf1eb736f440b27a0545f43a1727f2942
yeeshue99 <35947745+yeeshue99@users.noreply.github.com>
2023-05-28 14:58:12 -0700

    Touch database every day

---
+++ netlify/functions/touchDatabase.js
@@ -0,0 +1,17 @@
+const supabaseImport = require('@supabase/supabase-js');
+const { schedule } = require("@netlify/functions");
+
+const handler = async function (event, context) {
+    // let supabase = supabaseImport.createClient(process.env.REACT_APP_DATABASE_URL, process.env.REACT_APP_DATABASE_API_KEY)
+    let supabase = supabaseImport.createClient("https://xerzbigpkpuflykmhifk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlcnpiaWdwa3B1Zmx5a21oaWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA5ODk0ODgsImV4cCI6MTk5NjU2NTQ4OH0.w2w_81h4HgUFsrKp-abcE4deUzuem9UJvDQ53vKWmE8")
+
+
+    const skills = await supabase.from('Skills').select().order('name', { ascending: true })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(skills.data),
+    };
+};
+
+exports.handler = schedule("@daily", handler);
